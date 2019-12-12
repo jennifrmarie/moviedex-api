fae7f11ed130a7bffc531bc7e5d4d94eb5bb8b9f
Jennifer Ridgeway <jennifrmarie@icloud.com>
2019-12-12 08:28:27 -0500

    deleted console logs

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
 const MOVIEDEX = require('./movies-data.json')
 
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next){
   const apiToken = process.env.API_TOKEN
@@ -15,14 +18,12 @@ app.use(function validateBearerToken(req, res, next){
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized!' })
   }
-  console.log('validate bearer token middleware')
   next()
 })
 
 app.get('/movies', function handleGetMovies(req, res) {
   let result = MOVIEDEX.movies;
 
-  // console.log(num)
   if (req.query.country) {
     result = result.filter(movies =>
       movies.country.toLowerCase().includes(req.query.country.toLowerCase())
